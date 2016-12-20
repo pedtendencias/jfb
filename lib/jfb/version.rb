6bd81f539af1300fb3c75075ef79363df86b069b
rCamposCruz <rcampos@tendencias.com.br>
2016-12-20 19:23:42 -0200

    Updates version

--- lib/jfb/version.rb
+++ lib/jfb/version.rb
@@ -1,7 +1,11 @@
 module Jfb
   VERSION = "0.1.0"
+
 #Version 0.1.0
 # Creates proper interface with jdbc
 # creater interface for ResultSet Class
 # Handles exceptions and drivers
+
+#Version 0.1.1
+# Adds dependency which I had forgot.
 end
